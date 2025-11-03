8443529b2c854bbb5520a454b20325ae1127bce1
bulgakke <be.evgeniy.bulavin@gmail.com>
2025-11-03 23:12:10 +0400

    refactor: extract post timestamps into a partial

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def updated?(resource)
+    resource.updated_at != resource.created_at
+  end
 end
--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,5 +1,2 @@
 module PostsHelper
-  def updated?(post)
-    post.updated_at != post.created_at
-  end
 end
