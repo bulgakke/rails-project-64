f2d965bd64b8e5db9b69b041c3e38ef0cfdf376e
bulgakke <be.evgeniy.bulavin@gmail.com>
2025-11-03 21:52:59 +0400

    feat: make posts#index home page

--- test/system/pages_test.rb
+++
@@ -1,8 +0,0 @@
-require "application_system_test_case"
-
-class PagesTest < ApplicationSystemTestCase
-  test "visiting the home page" do
-    visit root_url
-    assert_selector "h1", text: "Hello world!"
-  end
-end
