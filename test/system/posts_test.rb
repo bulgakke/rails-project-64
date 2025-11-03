e0a23e6a88732721e4ec9e307dcd04252014e021
bulgakke <be.evgeniy.bulavin@gmail.com>
2025-11-03 21:55:15 +0400

    test: update system tests

--- test/system/posts_test.rb
+++ test/system/posts_test.rb
@@ -9,7 +9,7 @@ class PostsTest < ApplicationSystemTestCase
 
   test "visiting the index" do
     visit posts_url
-    assert_selector "h1", text: "Posts"
+    assert_selector "h2", text: "Posts"
   end
 
   test "should create post" do
@@ -22,12 +22,11 @@ class PostsTest < ApplicationSystemTestCase
     click_on "Create Post"
 
     assert_text "Post was successfully created"
-    click_on "Back"
   end
 
   test "should update Post" do
     visit post_url(@post)
-    click_on "Edit this post", match: :first
+    click_on "Edit", match: :first
 
     fill_in "Title", with: "test"
     fill_in "Body", with: "test"
@@ -35,12 +34,11 @@ class PostsTest < ApplicationSystemTestCase
     click_on "Update Post"
 
     assert_text "Post was successfully updated"
-    click_on "Back"
   end
 
   test "should destroy Post" do
     visit post_url(@post)
-    click_on "Destroy this post", match: :first
+    click_on "Delete", match: :first
 
     assert_text "Post was successfully destroyed"
   end
