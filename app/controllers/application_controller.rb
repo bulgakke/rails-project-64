5ad3aa56615f2603dcc1907e3601496d74b4638d
bulgakke <be.evgeniy.bulavin@gmail.com>
2025-11-12 21:11:48 +0400

    Revert "tool: endpoint for creating DB seeds"
    This reverts commit 126f414e037cb9652027f900be95a4d3aa2053a3.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,4 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
-
-  def seed
-    eval File.read(Rails.root.join('db/seeds.rb').to_s) # rubocop:disable Security/Eval
-
-    render plain: 'Seeds created'
-  end
 end
