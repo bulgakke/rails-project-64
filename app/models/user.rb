68079b2ceb197981495b15c3f221b201df24499d
bulgakke <be.evgeniy.bulavin@gmail.com>
2025-11-12 22:59:48 +0400

    fix: remove password reset

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,7 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable, :rememberable, :validatable
 
   has_many :posts, inverse_of: :creator, dependent: :destroy
   has_many :comments, class_name: 'PostComment', dependent: :destroy
