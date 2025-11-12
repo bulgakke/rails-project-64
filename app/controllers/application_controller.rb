126f414e037cb9652027f900be95a4d3aa2053a3
bulgakke <be.evgeniy.bulavin@gmail.com>
2025-11-12 19:32:29 +0400

    tool: endpoint for creating DB seeds
    Render doesn't allow SSH access and custom commands
    in the free tier; this is a work-around

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,10 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  def seed
+    eval File.read(Rails.root.join('db/seeds.rb').to_s) # rubocop:disable Security/Eval
+
+    render plain: 'Seeds created'
+  end
 end
